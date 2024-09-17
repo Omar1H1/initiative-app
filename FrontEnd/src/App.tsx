154b599e55e07884deba44ff06f0e52a0f06426b
Omer HAMAD <omerhamad1o1@gmail.com>
2024-09-17 13:15:29 +0200

    Done : connect the backend with front

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,22 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:8080/api/hello');
+      setMessage(result.data);
+    };
+    fetchData();
+  }, []);
+
+        console.log(message);
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-3xl font-bold underline">{message}</p>
+    </>
+  );
 }
