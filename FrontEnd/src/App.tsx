857b3cc5241cda2e546c7d2e818a6744e668a11b
Omer HAMAD <omerhamad1o1@gmail.com>
2024-11-08 15:06:06 +0100

    fix: fixed the logo, svg insted of png

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -6,22 +6,21 @@ import NotFound from "./components/NotFound";
 import NavBar from "./components/NavBar.tsx";
 import CodePage from "./components/CodePage.tsx";
 import IsLogged from "./service/LoginState.tsx";
-import {useState} from "react";
+import { useState } from "react";
 import Hero from "./components/Hero.tsx";
 
-
-
 const App = () => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ?? sessionStorage.getItem("token") ?? "",
+  );
 
-    const [token, setToken] = useState(localStorage.getItem("token") ?? sessionStorage.getItem("token") ?? '');
+  const value = [token, setToken];
 
-    const value = [token, setToken];
-
-    return (
-// @ts-ignore
-<IsLogged.Provider value={value}>
+  return (
+    // @ts-ignore
+    <IsLogged.Provider value={value}>
       <BrowserRouter>
-          <NavBar />
+        <NavBar />
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
           <Routes>
             <Route path="/" element={<Hero />} />
@@ -31,10 +30,10 @@ const App = () => {
             <Route path="/signup/submitcode" element={<CodePage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-       </div>
+        </div>
       </BrowserRouter>
-</IsLogged.Provider>
+    </IsLogged.Provider>
   );
-}
+};
 
 export default App;
--- FrontEnd/src/components/NavBar.tsx
+++ FrontEnd/src/components/NavBar.tsx
@@ -1,87 +1,91 @@
-import {useState, useContext} from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import { useNavigate } from 'react-router-dom';
-import logo from "../assets/logo.png";
+import { useState, useContext } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.svg";
 import isLogged from "../service/LoginState.tsx";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false);
-    const navigate = useNavigate();
-    const [token, setToken] = useContext(isLogged);
+  const [nav, setNav] = useState(false);
+  const navigate = useNavigate();
+  const [token, setToken] = useContext(isLogged);
 
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
+    setToken(null);
+    navigate("/");
+  };
 
-    const handleNav = () => {
-        setNav(!nav);
-    };
+  const handleLogin = () => {
+    navigate("/login");
+  };
 
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        sessionStorage.removeItem("token");
-        setToken(null);
-        navigate("/");
+  return (
+    <div className="bg-transparent flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <img
+        src={logo}
+        alt="logo"
+        className="w-48 h-auto bg-transparent bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg"
+      />
 
-    };
+      {token ? (
+        <ul className="hidden md:flex">
+          <li
+            onClick={handleLogout}
+            className="p-4 hover:bg-[#ffc0cb] rounded-xl m-2 cursor-pointer duration-300 hover:text-black"
+          >
+            Logout
+          </li>
+        </ul>
+      ) : (
+        <ul className="hidden md:flex">
+          <li
+            onClick={handleLogin}
+            className="p-4 hover:bg-blue-700 rounded-xl m-2 cursor-pointer duration-300 hover:text-white"
+          >
+            Login
+          </li>
+        </ul>
+      )}
 
-    const handleLogin = () => {
-        navigate("/login");
-    };
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
 
-    return (
-        <div className='bg-transparent flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-            <img src={logo} alt="logo"
-                 className="w-48 h-auto bg-transparent bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg"/>
-
-            {token ? (
-                <ul className='hidden md:flex'>
-                    <li
-                        onClick={handleLogout}
-                        className='p-4 hover:bg-[#ffc0cb] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
-                    >
-                        Logout
-                    </li>
-                </ul>
-            ) : (
-                <ul className='hidden md:flex'>
-                    <li
-                        onClick={handleLogin}
-                        className='p-4 hover:bg-blue-700 rounded-xl m-2 cursor-pointer duration-300 hover:text-white'
-                    >
-                        Login
-                    </li>
-                </ul>
-            )}
-
-            <div onClick={handleNav} className='block md:hidden'>
-                {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
-            </div>
-
-            <ul
-                className={
-                    nav
-                        ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-                        : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
-                }
-            >
-                <img src={logo} alt="Your Logo" className="w-48 h-auto bg-transparent"/>
-                {token ? (
-                    <li
-                        onClick={handleLogout}
-                        className='p-4 border-b rounded-xl hover:bg-[#ffc0cb] duration-300 hover:text-black cursor-pointer border-gray-600'
-                    >
-                        Logout
-                    </li>
-                ) : (
-                    <li
-                        onClick={handleLogin}
-                        className='p-4 border-b rounded-xl hover:bg-[#ffc0cb] duration-300 hover:text-black cursor-pointer border-gray-600'
-                    >
-                        Login
-                    </li>
-                )}
-            </ul>
-        </div>
-    );
+      <ul
+        className={
+          nav
+            ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
+        }
+      >
+        <img
+          src={logo}
+          alt="Your Logo"
+          className="w-48 h-auto bg-transparent"
+        />
+        {token ? (
+          <li
+            onClick={handleLogout}
+            className="p-4 border-b rounded-xl hover:bg-[#ffc0cb] duration-300 hover:text-black cursor-pointer border-gray-600"
+          >
+            Logout
+          </li>
+        ) : (
+          <li
+            onClick={handleLogin}
+            className="p-4 border-b rounded-xl hover:bg-[#ffc0cb] duration-300 hover:text-black cursor-pointer border-gray-600"
+          >
+            Login
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
