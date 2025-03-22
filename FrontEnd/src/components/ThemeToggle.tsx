3ec5c1f9a42dbe524edb7e72105ae844267c11d5
Omer HAMAD <omerhamad1o1@gmail.com>
2025-03-22 16:13:15 +0100

    feat: added profile logo to navbar
    also fixed bg color on some icons and their size

--- FrontEnd/src/components/ThemeToggle.tsx
+++ FrontEnd/src/components/ThemeToggle.tsx
@@ -4,50 +4,50 @@ import themeAtom from "../service/ThemeAtom";
 import { CiLight } from "react-icons/ci";
 import { FaMoon } from "react-icons/fa";
 
-
-
 const ThemeToggle = () => {
-    const [theme, setTheme] = useAtom(themeAtom);
+  const [theme, setTheme] = useAtom(themeAtom);
 
-    const handleToggle = () => {
-        const newTheme = theme === "light" ? "dark" : "light";
-        // @ts-ignore
-        setTheme(newTheme);
+  const handleToggle = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    // @ts-ignore
+    setTheme(newTheme);
 
-        if (newTheme === "dark") {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
+    if (newTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
 
-        localStorage.setItem("theme", newTheme);
-    };
+    localStorage.setItem("theme", newTheme);
+  };
 
-    useEffect(() => {
-        const storedTheme = localStorage.getItem("theme") || "light";
-        // @ts-ignore
-        setTheme(storedTheme);
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme") || "light";
+    // @ts-ignore
+    setTheme(storedTheme);
 
-        if (storedTheme === "dark") {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    }, [setTheme]);
+    if (storedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [setTheme]);
 
-    return (
-        <button onClick={handleToggle} className="p-2">
-            {theme === "dark" ? (
-                <CiLight
-                    className="text-4xl duration-500 ease-in-out hover:bg-yellow-500 rounded-lg"
-                />
-            ) : (
-                <FaMoon
-                    className="text-gray-900 p-2 text-4xl transition duration-500 ease-in-out  rounded-lg"
-                />
-            )}
-        </button>
-    );
+  return (
+    <button onClick={handleToggle} className="p-2">
+      {theme === "dark" ? (
+        <CiLight
+          size={40}
+          className="text-4xl duration-500 ease-in-out hover:bg-yellow-500 rounded-lg"
+        />
+      ) : (
+        <FaMoon
+          size={40}
+          className="text-gray-900 p-2 text-4xl transition hover:bg-gray-400 duration-500 ease-in-out  rounded-lg"
+        />
+      )}
+    </button>
+  );
 };
 
 export default ThemeToggle;
